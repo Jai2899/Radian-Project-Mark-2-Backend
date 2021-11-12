1cba7e741b9c026aaab23baee1260aa5cd73767a
Jai2899 <gjayasriram@gmail.com>
2021-11-11 21:22:30 -0500

    Backend Code V1

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { graphqlHTTP } = require("express-graphql");;
+const { graphqlHTTP } = require("express-graphql");
 const mongoose = require("mongoose");
-const graphQlSchema = require('./graphql/schema/index');
-const graphQlResolvers = require('./graphql/resolvers/index');
+const graphQlSchema = require("./graphql/schema/index");
+const graphQlResolvers = require("./graphql/resolvers/index");
 const app = express();
 app.use(bodyParser.json());
 app.use(
@@ -20,7 +20,7 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@nodetuts.csejm.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(3000);
+    app.listen(8000);
   })
   .catch((err) => {
     console.log(err);
